accff94116805170470b54e446257e542b74f10c
RuudV <ruud.vissers@student.fontys.nl>
2018-04-04 20:06:54 +0200

    fixed beating a peasant with accel sensor

--- app/src/main/java/com/example/ruud/medievaldoctor/battle.java
+++ app/src/main/java/com/example/ruud/medievaldoctor/battle.java
@@ -1,16 +1,31 @@
 package com.example.ruud.medievaldoctor;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
 
 import java.util.Random;
 
-public class battle extends AppCompatActivity {
+public class battle extends AppCompatActivity implements SensorEventListener {
+
+    private boolean isPeasantDead = false;
+
+    private long lastUpdate = -1;
+    private float x, y, z;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 3200;
+    private SensorManager sensorMgr;
+    private boolean isShaked = false;
 
     private ProgressBar peasantHp;
     private ProgressBar doctorHp;
@@ -29,11 +44,73 @@ public class battle extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
 
+
         peasantHp = (ProgressBar) findViewById(R.id.enemyProgressBar);
         doctorHp = (ProgressBar) findViewById(R.id.doctorProgressBar);
         //setting the bar to 100
         peasantHp.setProgress(100);
         doctorHp.setProgress(100);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorMgr == null) {
+            sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+            boolean accelSupported = sensorMgr.registerListener(this, sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+
+            if (!accelSupported) {
+                // on accelerometer on this device
+                sensorMgr.unregisterListener(this, sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+            }
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            // only allow one update every 100ms.
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                x = sensorEvent.values[0];
+                y = sensorEvent.values[1];
+                z = sensorEvent.values[2];
+
+                float speed = Math.abs(x+y+z - last_x - last_y - last_z)
+                        / diffTime * 10000;
+                if (speed > SHAKE_THRESHOLD) {
+                    // yes, this is a shake action! Do something about it!
+                    isShaked = true;
+                    //TODO: implement new intent;
+                    if(isPeasantDead)
+                    {
+                        //you have beaten the peasant to dead continue to the previous map
+                        startActivity(new Intent(battle.this, map.class));
+
+                    }
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    protected void onPause() {
+        if (sensorMgr != null) {
+            sensorMgr.unregisterListener(this, sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+            sensorMgr = null;
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 
     //@Override
@@ -55,7 +132,17 @@ public class battle extends AppCompatActivity {
     {
         if(doctorHp.getProgress() < 10 || peasantHp.getProgress() < 10)
         {
-            //TODO: send back to map activity
+            if(doctorHp.getProgress() < 10)
+            {
+                //TODO: peasant won
+                //TODO: popup 1 level setback
+                //TODO: lose some bitcoin
+            }
+            else
+            {
+                Toast.makeText(this, "Shake your phone to beat the shit out of this peasant and grab his medieval bitcoin", Toast.LENGTH_LONG).show();
+                isPeasantDead = true;
+            }
         }
         else if(doctorStrength >= peasantStrength)
         {
@@ -72,6 +159,17 @@ public class battle extends AppCompatActivity {
         if(doctorHp.getProgress() < 10 || peasantHp.getProgress() < 10)
         {
             //TODO: send back to map activity
+            if(doctorHp.getProgress() < 10)
+            {
+                //TODO: peasant won
+                //TODO: popup 1 level setback
+                //TODO: lose some bitcoin
+            }
+            else
+            {
+                Toast.makeText(this, "Shake your phone to beat the shit out of this peasant and grab his medieval bitcoin", Toast.LENGTH_LONG).show();
+                isPeasantDead = true;
+            }
         }
         else if(doctorSeduce > peasantSeduce)
         {
@@ -89,6 +187,17 @@ public class battle extends AppCompatActivity {
         if(doctorHp.getProgress() < 10 || peasantHp.getProgress() < 10)
         {
             //TODO: send back to map activity
+            if(doctorHp.getProgress() < 10)
+            {
+                //TODO: peasant won
+                //TODO: popup 1 level setback
+                //TODO: lose some bitcoin
+            }
+            else
+            {
+                Toast.makeText(this, "Shake your phone to beat the shit out of this peasant and grab his medieval bitcoin", Toast.LENGTH_LONG).show();
+                isPeasantDead = true;
+            }
         }
         else if(doctorSpeech > peasantSpeech)
         {
@@ -99,6 +208,4 @@ public class battle extends AppCompatActivity {
             doctorHp.setProgress((doctorHp.getProgress()) - 10);
         }
     }
-
-
 }
